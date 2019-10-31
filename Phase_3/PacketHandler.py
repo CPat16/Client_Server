7acc3521c542ff193b7b60770c03e81d6d286950
Christian Patterson <fondu16@gmail.com>
2019-10-30 21:11:40 -0400

    Initial Phase 3 (rdt 2.2 attempt)

---
+++ Phase_3/PacketHandler.py
@@ -0,0 +1,74 @@
+# This file contains the Packet class which handles packing functions
+import struct
+
+class Packet():
+  def __init__(self, seq_num=-1, data=b''):
+    self.seq_num = seq_num
+    self.data = data
+    if(seq_num >= 0):
+      self.csum = self.checksum(msg)
+    else:
+      self.csum = -1
+    self.fmt = '!H' + 'B'*len(self.data) + 'H'
+  def __repr__(self):
+    return "\nseq_num: {0}\n csum: {1}\n data: {2}\n".format(self.seq_num, self.csum, self.data)
+
+  def pkt_pack(self):
+    if isinstance(self.data, str):
+      data = self.data.encode()
+    else:
+      data = self.data
+    return struct.pack(self.fmt, self.seq_num, *data, self.csum)
+
+  def pkt_unpack(self, packed):
+    self.seq_num = int.from_bytes(packed[0:2], byteorder='big', signed=False)
+    self.data = packed[2:(len(packed)-2)]
+    self.csum = int.from_bytes(packed[(len(packed)-2):len(packed)], byteorder='big', signed=False)
+    #return struct.unpack(self.fmt, packed)
+
+  def carry_around_add(self, a, b):
+    c = a + b
+    return (c & 0xffff) + (c >> 16)
+
+  def checksum(self, msg):
+    csum = 0
+    if isinstance(msg, str):
+      msg = msg.encode()
+    if isinstance(msg, (bytearray, bytes)):
+      msg = int.from_bytes(msg, byteorder='big', signed=False)
+    #for _ in range(0, msg.bit_length()//16 + 1, 1):
+    while msg != 0:
+      next_byte = msg & 0xffff
+      csum = self.carry_around_add(csum, next_byte)
+      # print(s, ':', bin(s))
+      msg = msg >> 16
+      # print(bin(msg))
+    return ~csum & 0xffff
+
+if __name__ == "__main__":
+  msg = b'\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01\x00\x01'
+  # msg = "ACK"
+  # int_msg = int.from_bytes(msg, byteorder='big', signed=False)
+  # print(bin(int_msg))
+
+  pkt = Packet(1, msg)
+
+  print(pkt)
+
+  packed = pkt.pkt_pack()
+  print(packed)
+
+
+  #unpacked = pkt.pkt_unpack(packed)
+  # unpacked = struct.unpack('!H', packed)
+
+  """ print(int.from_bytes(packed[0:2], byteorder='big', signed=False))
+  print(packed[2:(len(packed)-2)])
+  print(int.from_bytes(packed[(len(packed)-2):len(packed)], byteorder='big', signed=False)) """
+
+  new_pkt = Packet()
+  print("New pkt before:", new_pkt)
+
+  new_pkt.pkt_unpack(packed)
+  print("New pkt unpacked:", new_pkt)
+  # print("Message:", new_pkt.data.decode())
---
+++ Phase_3/udp_server.py
@@ -0,0 +1,186 @@
+# This file implements the server process
+import socket
+import io
+import os
+import sys
+from time import sleep
+from threading import Thread
+
+from PacketHandler import Packet
+# import udp_client
+
+class Server(Thread):
+  def __init__(self):
+    """
+    Initializes Server Process
+    """
+    Thread.__init__(self)                   # initializes as thread
+    host_ip = '127.0.0.1'
+    server_port = 20001
+    server_addr = (host_ip, server_port)    # address of server (IP, Port)
+
+    self.pkt_size = 1024                                # packet size
+    self.header_size = 4                                # bytes of header data
+    self.data_size = self.pkt_size - self.header_size   # Size of data in packet
+    self.img_to_send = 'hello.jpg'                      # relative path of image to send
+    self.img_save_to = 'server_img.jpg'                 # filename to save received image
+
+    # create UDP server socket
+    self.server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    self.server_socket.settimeout(10)       # Recieving sockets timeout after 10 seconds
+
+    # bind to the socket
+    try:
+      self.server_socket.bind(server_addr)
+      print("Server bound to port", server_port)
+
+    except:
+      print("Server failed bind to port", server_port)
+
+  def send_img(self, filename):
+    """
+    Sends the image packet by packet
+
+    Parameters:
+      filename - file to send
+    """
+    recv_data = b''       # bytes for data received
+    recv_pkt = Packet()   # init empty packet for received data
+    read_data = b''       # byte string data read from file
+    seq_num = 0           # init sequence number
+    
+    # open file to be sent
+    print("Server: Sending image to client")
+    with open(filename, 'rb') as img:
+      read_data = img.read(self.data_size)
+
+      # send data until end of file reached
+      while read_data:
+        # pack and send
+        send_pkt = Packet(seq_num=seq_num, data=read_data)
+        packed = send_pkt.pkt_pack()
+        self.server_socket.sendto(packed, self.client_addr)
+        
+        # wait for ACK
+        recv_data = self.server_socket.recv(self.pkt_size)
+        recv_pkt.pkt_unpack(recv_data)
+        # Received NAK or incorrect ACK
+        if recv_pkt.seq_num != seq_num or recv_pkt.csum != recv_pkt.checksum(recv_pkt.data):
+          print("Server: Received NAK or corrupted ACK, Resending...")
+        # ACK is OK, move to next data and sequence
+        else:
+          seq_num ^= 1
+          read_data = img.read(self.data_size)
+   
+  def recv_img(self, filename):
+    """
+    Receive an image packet by packet
+    Saves image to specified file
+
+    Parameters:
+      filename - file location to save image
+    """
+    recv_data = b''           # packet of byte string data
+    save_data = b''           # data to be saved to file
+    img_not_recvd = True      # flag to indicate if image has been recieved
+    exp_seq = 0               # expected sequence number initially 0
+    pkt = Packet()
+
+    # get image data from server until all data received
+    while True:
+      try:
+        if img_not_recvd:
+          print("Server: Ready to receive image", flush=True)
+        recv_data = self.server_socket.recv(self.pkt_size)
+        
+        pkt.pkt_unpack(recv_data)
+        if pkt.seq_num != exp_seq or pkt.csum != pkt.checksum(pkt.data):
+          ack = Packet(exp_seq^1, "ACK")
+          print("Server: Sending ACK for incorrect packet")
+        else:
+          save_data += pkt.data
+          ack = Packet(exp_seq, "ACK")
+          exp_seq ^= 1
+
+        self.server_socket.sendto(ack, self.client_addr)
+        
+        if img_not_recvd:
+          img_not_recvd = False       # img data began streaming if it reaches this point
+      
+      except socket.timeout:
+        # if image not recieved yet, keep waiting
+        if img_not_recvd:
+          pass
+        # image has been recieved
+        else:
+          # write data into a file
+          with open(filename, 'wb+') as server_img:
+            server_img.write(save_data)
+          print("Server: Received and saved image", flush=True)
+          break   # exit loop
+
+  def run(self):
+    """
+    Runs when Server process has started
+    """
+    print("Server: Started")
+    i = 0       # index of timeouts
+    
+    while True:
+      try:
+        print("Server: Ready", flush=True)
+        # get request and address from client
+        msg = b''           # set message to empty string
+        recv_data = b''     # set received data to empty string
+        msg_pkt = Packet()  # init empty packet
+        (recv_data, self.client_addr) = self.server_socket.recvfrom(self.pkt_size)
+        i = 0   # reset timeout index
+        msg_pkt.pkt_unpack(recv_data)
+        msg = msg_pkt.data.decode()
+        print("Server: Client request:", msg, flush=True)
+
+      except socket.timeout:
+        i = i + 1
+        if(i < 6):
+          pass
+        # if the server has waited through 6 timeouts (60 seconds), exit
+        else:
+          print("Server: I'm tired of waiting", flush=True)
+          break
+
+      # if any message recieved, then service it
+      if msg:
+        # ------------------ Send image to client ------------------
+        if msg == "download":
+          self.send_img(self.img_to_send)
+
+        # ------------------ Get image from client ------------------
+        elif msg == "upload":
+          self.recv_img(self.img_save_to)
+
+        # ------------------ Exit server process ------------------
+        elif msg == "exit":
+          print("Server: Exiting...")
+          break
+        
+        # ------------------ Handle invalid request ------------------
+        else:
+          print("Server: Received invalid request:", msg)
+
+    # close socket when finished
+    self.server_socket.close()
+
+
+if __name__ == "__main__":
+  # Runs process
+  serv_proc = Server()  # init server thread
+  #client_thread = Thread(target=udp_client.client) # for testing
+
+  serv_proc.start()     # start server thread
+  #client_thread.start() # for testing
+
+  serv_proc.join()      # wait for server thread to end
+  #client_thread.join()  # for testing
+
+  print("Finished transmission, closing...")
